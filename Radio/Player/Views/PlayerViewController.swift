9a15e14de7ff7856ffc07a4514a424373041fb38
Andrey <andreyly@bk.ru>
2021-02-19 23:11:54 +0300

    start project

--- Radio/Player/Views/PlayerViewController.swift
+++ Radio/Player/Views/PlayerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PlayerViewController.swift
 //  Radio
 //
 //  Created by Андрей Лапин on 19.02.2021.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlayerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
     }
 
-
 }
-
