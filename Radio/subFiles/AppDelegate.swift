15ff6f396079c6c19d72c571db3a3347eea64eae
Andrey <andreyly@bk.ru>
2021-02-21 13:29:41 +0300

    add playback music game

--- Radio/subFiles/AppDelegate.swift
+++ Radio/subFiles/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let session = AVAudioSession.sharedInstance()
+        do{
+            try session.setActive(true)
+            try session.setCategory(AVAudioSession.Category.playback)
+        } catch{
+            print(error.localizedDescription)
+        }
         return true
     }
 
